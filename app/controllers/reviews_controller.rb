b3234fdb8e62d68d7d43bc15d7eacaf021ea9049
Denis Bondin <dsbondin@gmail.com>
2017-11-13 00:47:31 +0000

    still working on reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,14 @@
 class ReviewsController < ApplicationController
 
+  get '/reviews/:id/new' do
+    # raise params.inspect
+    if logged_in?
+      @product = Product.find(params[:id])
+      erb :'reviews/new'
+    else
+      session[:notice]  = "You must be logged in to add a product review"
+      redirect '/login'
+    end
+  end
 
 end
