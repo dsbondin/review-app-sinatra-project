c3b0c07a9925066600a8a24ec597e7f45f3ba9eb
github username <github email address>
2018-02-22 17:53:27 -0500

    deploying to heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,13 @@
-configure :development do
-  ENV['SINATRA_ENV'] ||= "development"
+ENV['SINATRA_ENV'] ||= "development"
 
-  require 'bundler/setup'
-  Bundler.require(:default, ENV['SINATRA_ENV'])
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
 
-  ActiveRecord::Base.establish_connection(
-    :adapter => "sqlite3",
-    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-  )
-end
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
 
 require_all 'app'
 
