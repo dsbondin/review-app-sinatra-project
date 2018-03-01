01151964721d79797d308c4ceee063fb3823017b
github username <github email address>
2018-03-01 07:16:44 -0500

    deploying to heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,14 @@
-require 'rubygems'
+ENV['SINATRA_ENV'] ||= "development"
+
 require 'bundler/setup'
-require 'sinatra'
-require 'active_record'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+require_all 'app'
 
 configure :production do
    db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
